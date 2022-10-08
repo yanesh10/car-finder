51c0c8f4bb5bf3d78ae8d24562e7a4c04b439f66
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-10-08 18:36:02 +0400

    Initial commit for api gateway

---
+++ api-gateway/src/test/java/mu/yanesh/car/finder/api/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.yanesh.car.finder.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
