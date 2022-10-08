0c21e76c3fa4657c8c9746878aa5274ce6d85b2e
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-10-08 14:19:33 +0400

    Initial commit

---
+++ extractor/src/test/java/mu/car/finder/extractor/ExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.car.finder.extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
