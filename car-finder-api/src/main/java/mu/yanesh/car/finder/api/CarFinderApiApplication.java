cb15fa7ceb5a0c171be173f2752c895cfbd42d9f
Yanesh Caussy <yanesh.caussy@tribalworldwide.co.uk>
2022-10-09 19:54:56 +0400

    Initial commit api

---
+++ car-finder-api/src/main/java/mu/yanesh/car/finder/api/CarFinderApiApplication.java
@@ -0,0 +1,13 @@
+package mu.yanesh.car.finder.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFinderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarFinderApiApplication.class, args);
+    }
+
+}
---
+++ car-finder-api/src/test/java/mu/yanesh/car/finder/api/CarFinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mu.yanesh.car.finder.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFinderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
